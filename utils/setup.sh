835c63a77969e71d5ad2f7cf97c36ef13f08e597
Vincent C. Mader <vincent.mader@icloud.com>
2022-08-15 13:53:48 +0200

    updated python requirements & setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -3,5 +3,7 @@
 cd "$(dirname $0)"
 PATH_TO_SCRIPTS=$(pwd)
 
+pip3 install -r ../requirements.txt
+
 python3 "$PATH_TO_SCRIPTS/create_sprites.py" ducks
 python3 "$PATH_TO_SCRIPTS/create_sprites.py" miniducks
