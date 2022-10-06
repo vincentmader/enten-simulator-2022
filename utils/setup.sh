efed668db91de566b90cdc3520a80c5179818af9
Vincent C. Mader <vincent.mader@icloud.com>
2022-10-06 17:55:37 +0200

    added sprites for more animals

--- utils/setup.sh
+++ utils/setup.sh
@@ -7,3 +7,7 @@ pip3 install -r ../requirements.txt
 
 python3 "$PATH_TO_SCRIPTS/create_sprites.py" ducks
 python3 "$PATH_TO_SCRIPTS/create_sprites.py" miniducks
+python3 "$PATH_TO_SCRIPTS/create_sprites.py" monkeys
+python3 "$PATH_TO_SCRIPTS/create_sprites.py" ostriches
+python3 "$PATH_TO_SCRIPTS/create_sprites.py" penguins
+python3 "$PATH_TO_SCRIPTS/create_sprites.py" bears
